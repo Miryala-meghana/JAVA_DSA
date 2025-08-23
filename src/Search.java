aa3d25c60e6c78584df0f83792c9b6514dff0f1f
Maggie <miryalameghana143@gmail.com>
2025-08-23 20:59:08 +0530

    corrected the logic in recurssive binay search

--- src/Search.java
+++ src/Search.java
@@ -5,23 +5,34 @@ import static java.util.Arrays.sort;
 public class Search {
     public static void main(String args[])
     {
-        int num[]={2,4,5,6,9};
-        int key=9;
+        int num[]={2,4,5,6,9,10,12,34,45,56};
+        int key=12;
 
         int res=linesrSearch(num,key);
         int res1=binarySearch(num,key);
+        int res2=recursiveBinarySearch(num,key,0,num.length-1);
+
+//        searching via linear search
         if(res==-1)
             System.out.println("not found");
         else
-            System.out.println("found at index "+res);
-
+            System.out.println("found at index :linear"+res);
+//        searching via Binary linear search
         if(res1==-1)
             System.out.println("not found");
         else
-            System.out.println("found at index "+res);
+            System.out.println("found at index :binary"+res1);
+
+//        searching via recursive binary search
+        if(res2==-1)
+            System.out.println("not found");
+        else
+            System.out.println("found at index :rec binary"+res2);
     }
 
 
+
+//method for linear search
 
     public static int linesrSearch(int[] num, int key) {
 
@@ -33,7 +44,7 @@ public class Search {
         return -1;
     }
 
-
+//method for Binary search.
 
     private static int binarySearch(int[] num, int key) {
 
@@ -58,5 +69,24 @@ public class Search {
 
        return -1;
     }
+
+
+//    method for recursive binary search
+private static int recursiveBinarySearch(int[] num, int key,int left,int right) {
+//    int num[]={2,4,5,6,9,10,12,34,45,56};
+            if (left>right)
+               return -1;
+            else{
+            int mid=(left+right)/2;
+            if (num[mid]==key){
+                System.out.println(mid +" "+key);
+                return mid;}
+            else if(num[mid] < key)
+                return  recursiveBinarySearch(num,key,mid+1,right);
+            else
+                return recursiveBinarySearch(num,key,left,mid-1);
+            }
+
+}
 
 }
